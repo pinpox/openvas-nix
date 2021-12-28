fe9e8d79c976ffaab456d4222be2bad6446120c6
Pablo Ovelleiro Corral <mail@pablo.tools>
2021-12-28 12:40:20 +0100

    Add newer version of gvm-libs

--- flake.nix
+++ flake.nix
@@ -65,6 +65,46 @@
               ];
 
               # meta = with lib; { };
+
+            };
+
+            gvm-libs = stdenv.mkDerivation rec {
+              pname = "gvm-libs";
+              version = "21.4.3";
+
+              src = fetchFromGitHub {
+                owner = "greenbone";
+                repo = pname;
+                rev = "v${version}";
+                sha256 = "sha256-1NVLGyUDUnOy3GYDtVyhGTvWOYoWp95EbkgTlFWuxE8=";
+              };
+
+              nativeBuildInputs = [ cmake pkg-config ];
+
+              buildInputs = [
+                glib
+                glib-networking
+                gnutls
+                gpgme
+                hiredis
+                libgcrypt
+                freeradius
+                libnet
+                libpcap
+                libssh
+                libuuid
+                libxml2
+                zlib
+              ];
+
+              meta = with lib; {
+                description =
+                  "Libraries module for the Greenbone Vulnerability Management Solution";
+                homepage = "https://github.com/greenbone/gvm-libs";
+                license = with licenses; [ gpl2Plus ];
+                maintainers = with maintainers; [ fab ];
+                platforms = platforms.linux;
+              };
             };
 
             openvas-scanner = pkgs.stdenv.mkDerivation rec {
@@ -99,7 +139,7 @@
                 glib
                 gnutls
                 libtasn1
-                gvm-libs
+self.packages.${system}.gvm-libs
                 libssh
                 libpcap
                 libksba
